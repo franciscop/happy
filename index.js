86e43335010ad076a208250a7ccfbfd55850e2dc
Francisco Presencia <public@francisco.io>
2019-04-30 16:49:45 +0200

    Commited on 2019-04-30T14:49:45.130Z

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const atocha = require("atocha");
+// const atocha = require("atocha");
 const listr = require("listr");
 
 const time = () => new Date().toISOString();
@@ -8,6 +8,28 @@ const time = () => new Date().toISOString();
 const swear = require("swear");
 const { promisify } = require("util");
 const exec = promisify(require("child_process").exec);
+
+const atocha = (command, buffer = 10) => {
+  const maxBuffer = buffer * 1024 * 1024;
+  return swear(
+    exec(command, { maxBuffer }).then(out => {
+      console.log(out.stderr);
+      if (out.stderr) throw new Error(out.stderr);
+      return out.stdout.trim();
+    })
+  );
+};
+
+(async () => {
+  try {
+    await atocha(`git add . -A`);
+    await atocha(`git commit -m "Commited on ${time()}"`);
+    await atocha(`git pull origin master`).catch(() => {});
+    await atocha(`git push`);
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 const tasks = new listr([
   {
