6d813a6820008663936c120c4331063c8e840112
Francisco Presencia <public@francisco.io>
2019-04-30 16:54:12 +0200

    Commited on 2019-04-30T14:54:12.774Z

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const atocha = (command, buffer = 10) => {
 })();
 
 const ridiculous = err => {
+  if (/nothing to commit, working tree clean/.test(err.message)) return;
   if (/branch\s+master\s+-> FETCH_HEAD/.test(err.message)) return;
   if (/master -> master/.test(err.message)) return;
   throw err;
