c3a17163e942c102ead46ff4c59eed7c3e9f678a
Francisco Presencia <public@francisco.io>
2019-04-30 16:47:31 +0200

    Commited on 2019-04-30T14:47:31.703Z

--- index.js
+++ index.js
@@ -19,30 +19,41 @@ const atocha = (command, buffer = 10) => {
   );
 };
 
-const tasks = new listr([
-  {
-    title: "Adding",
-    task: async () => await atocha(`git add . -A`)
-  },
-  {
-    title: "Commiting",
-    task: async () => await atocha(`git commit -m "Commited on ${time()}"`)
-  },
-  {
-    title: "Pulling from master",
-    task: async () =>
-      await atocha(`git pull origin master`).catch(err => {
-        // Accept this "error"
-        if (/Already up to date/.test(err.message)) return;
-        throw err;
-      })
-  },
-  {
-    title: "Pushing",
-    task: async () => await atocha(`git push`)
+(async () => {
+  try {
+    await atocha(`git add . -A`);
+    await atocha(`git commit -m "Commited on ${time()}"`);
+    await atocha(`git pull origin master`);
+    await atocha(`git push`);
+  } catch (error) {
+    console.log(error);
   }
-]);
+})();
 
-tasks.run().catch(err => {
-  console.error("ERROR:", err);
-});
+// const tasks = new listr([
+//   {
+//     title: "Adding",
+//     task: async () => await atocha(`git add . -A`)
+//   },
+//   {
+//     title: "Commiting",
+//     task: async () => await atocha(`git commit -m "Commited on ${time()}"`)
+//   },
+//   {
+//     title: "Pulling from master",
+//     task: async () =>
+//       await atocha(`git pull origin master`).catch(err => {
+//         // Accept this "error"
+//         if (/Already up to date/.test(err.message)) return;
+//         throw err;
+//       })
+//   },
+//   {
+//     title: "Pushing",
+//     task: async () => await atocha(`git push`)
+//   }
+// ]);
+//
+// tasks.run().catch(err => {
+//   console.error("ERROR:", err);
+// });
