4db2c103e4d9aa10007db924a5c9e320de20c61b
Francisco Presencia <public@francisco.io>
2019-12-07 16:43:42 +0900

    Saved on 2019-12-07T07:43:42Z

--- src/lint.js
+++ src/lint.js
@@ -4,7 +4,7 @@ const { read } = require("files");
 module.exports = cli => ({
   title: "Linting",
   skip: async ctx => {
-    if (!ctx.pkg.scripts.linter) return true;
+    if (!ctx.pkg.scripts.linter) return "No 'lint' script found";
   },
   task: async ctx => {
     return await cmd(ctx.pkg.scripts.linter);
--- src/pull.js
+++ src/pull.js
@@ -2,13 +2,13 @@ const cmd = require("atocha");
 const { wtf } = require("./helpers");
 
 module.exports = cli => ({
-  title: "Downloading latest version",
+  title: "Downloading latest",
   skip: async () => {
     const status = await cmd(`git status`);
     const ahead = /Your branch is ahead of/.test(status);
     if (ahead) return true;
     const updated = /Your branch is up to date with/.test(status);
-    if (updated) return true;
+    if (updated) return "Up to date";
   },
   task: async () => await cmd(`git pull origin master`).catch(wtf)
 });
