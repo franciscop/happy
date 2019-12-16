11452e83994967bab49ab685dcf30868c24b076d
Francisco Presencia <public@francisco.io>
2019-12-16 21:46:45 +0900

    Saved on 2019-12-16T12:46:45Z

--- src/build.js
+++ src/build.js
@@ -7,7 +7,7 @@ let test = "jest";
 module.exports = cli => ({
   title: "Building project",
   skip: async ctx => {
-    if (ctx.now) return "--now skips the build step";
+    // if (ctx.now) return "--now skips the build step";
     if (!ctx.pkg.scripts.build) return "no build script found";
   },
   task: async ctx => cmd("npm run build").catch(stderrok)
