d8acdcef652620d63d36d76d6b04bff87b92172c
Francisco Presencia <public@francisco.io>
2019-12-16 21:46:15 +0900

    Saved on 2019-12-16T12:46:15Z

--- src/build.js
+++ src/build.js
@@ -7,7 +7,7 @@ let test = "jest";
 module.exports = cli => ({
   title: "Building project",
   skip: async ctx => {
-    if (ctx.now) return true;
+    if (ctx.now) return "Fast fast";
     if (!ctx.pkg.scripts.build) return true;
   },
   task: async ctx => cmd("npm run build").catch(stderrok)
