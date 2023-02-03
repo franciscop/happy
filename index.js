b718b149774b476e96e961a335142717167a75ca
Francisco <public@francisco.io>
2023-02-03 13:56:13 +0900

    Fixed ESM imports

--- index.js
+++ index.js
@@ -88,6 +88,8 @@ if (flags.publish) {
 
 const tasks = new listr(action.map((task) => task({ flags, input })));
 
+console.log("TASKS");
+
 analyze()
   .then((ctx) => tasks.run(ctx))
   .catch((error) => console.error(error));
